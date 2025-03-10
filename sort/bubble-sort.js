ecaa07368799dfaa6cb617c708f8fa7f317c6fc6
Yana <annetart1994@gmail.com>
2025-03-10 23:10:53 +0200

    feat: add bubble sort

---
+++ sort/bubble-sort.js
@@ -0,0 +1,22 @@
+const bubbleSort = (array) => {
+    let last = array.length - 1;
+    
+    for (let i = 0; i < last; i++) { 
+        let swapped = false;
+        
+        for (let j = 0; j < last - i; j++) { 
+            // за зростанням >, за спаданням <
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                swapped = true;
+            }
+        }
+        
+        if (!swapped) break; 
+    }
+    
+    return array;
+};
+
+const array = [5, 7, 8, 89, 45, 1, 6, 3]
+console.log(bubbleSort(array));
